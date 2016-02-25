d4d03565e5c7c332ea0f5dd80646ada3659e7d8a
Chris Bonser <chris@khorus.com>
2016-02-24 21:04:48 -0600

    basic business-time-button

--- app/components/business-time-button.js
+++ app/components/business-time-button.js
@@ -1,9 +1,16 @@
+// Don't define components here or they are not extendable by the consuming
+// app
+
 import Ember from 'ember';
+import layout from 'magic-button/templates/components/business-time-button';
 
 export default Ember.Component.extend({
+  layout: layout,
   itsOn: false,
+  delay: 1000,
   tagName: 'button',
   click() {
-    this.toggleProperty('itsOn');
+    this.set('itsOn', true);
+    Ember.run.later(() => this.set('itsOn', false), this.get('delay'));
   }
 });
