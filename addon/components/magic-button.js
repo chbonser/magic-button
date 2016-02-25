fc4a47fe964f459d11b0b6c44338a516f3b701b7
Chris Bonser <chris@khorus.com>
2016-02-24 20:39:07 -0600

    better magic button

--- addon/components/magic-button.js
+++ addon/components/magic-button.js
@@ -5,7 +5,9 @@ export default Ember.Component.extend({
   layout: layout,
   tagName: 'button',
   magicIsHappening: false,
+  delay: 1000,
   click() {
     this.toggleProperty('magicIsHappening');
+    Ember.run.later(() => this.toggleProperty('magicIsHappening'), this.get('delay'));
   }
 });
